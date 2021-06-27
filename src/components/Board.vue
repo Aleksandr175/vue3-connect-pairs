3a3a4ab03bee5edd3b8d06aae9da92c92d144cee
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2021-06-27 20:37:51 +0300

    Add board

---
+++ src/components/Board.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="board">
+    <BoardItem v-for="(cell, index) in cells" :key="cell + '-' + index" :icon="cell" />
+  </div>
+</template>
+
+<script>
+import BoardItem from "@/components/BoardItem";
+
+export default {
+  name: 'Board',
+  
+  components: {
+    BoardItem
+  },
+  
+  data() {
+    return {
+      cells: [3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3],
+    };
+  }
+}
+
+</script>
+
+<style>
+.board {
+  display: flex;
+  flex-wrap: wrap;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/BoardItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="board-item">
+    {{ icon ? icon : '' }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BoardItem',
+  
+  props: {
+    icon: {
+      type: Number,
+      required: false,
+      default: 0,
+    }
+  }
+}
+
+</script>
+
+<style>
+.board-item {
+  border: 1px solid #2c3e50;
+  width: 48px;
+  height: 48px;
+}
+</style>
