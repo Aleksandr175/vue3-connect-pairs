3bc330550f9f04ac6ccbe9df8ba9617ca9cba73b
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2021-07-05 23:32:36 +0300

    Add path logic

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="board">
-    <BoardItem v-for="(cell, index) in cells" :key="cell + '-' + index" :icon="cell" />
+    <BoardItem
+      v-for="(cell, index) in cells"
+      :key="cell + '-' + index"
+      :icon="cell"
+      @mousedown="mousedown(index)"
+      @mouseup="mouseup(index)"
+      @mousemove="go(index)"
+      :selected="checkRoad(index)"
+      :closed="checkClosedRoad(index)"
+    />
   </div>
 </template>
 
 <script>
 import BoardItem from "@/components/BoardItem";
+import { ref } from 'vue';
 
 export default {
   name: 'Board',
@@ -14,10 +24,55 @@ export default {
     BoardItem
   },
   
-  data() {
+  setup() {
+    const cells = ref([3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3]);
+    const path = ref([]);
+    const size = ref(4);
+    const closedPath = ref([]);
+    
+    const mousedown = (index) => {
+      path.value = [];
+      
+      if (cells.value[index]) {
+        path.value.push(index);
+      }
+    }
+    
+    const mouseup = (index) => {
+      if (index !== path.value[0] && cells.value[index] === cells.value[path.value[0]]) {
+        closedPath.value = closedPath.value.concat(path.value);
+      }
+      
+      path.value = [];
+    }
+    
+    const go = (index) => {
+      if (path.value.length) {
+        const lastIndex = path.value[path.value.length - 1];
+        
+        if (Math.abs(lastIndex - index) === 1 || Math.abs(lastIndex - index) === size.value) {
+          path.value.push(index);
+        }
+      }
+    }
+    
+    const checkRoad = (index) => {
+      return path.value.findIndex(p => p === index) > -1;
+    }
+    
+    const checkClosedRoad = (index) => {
+      return closedPath.value.findIndex(p => p === index) > -1;
+    }
+
     return {
-      cells: [3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3],
-    };
+      cells,
+      mousedown,
+      mouseup,
+      go,
+      path,
+      checkRoad,
+      checkClosedRoad,
+    }
   }
 }
 
--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="board-item">
+  <div :class="'board-item ' + (selected ? 'selected ' : '') + (closed ? ' closed' : '')">
     {{ icon ? icon : '' }}
   </div>
 </template>
@@ -14,6 +14,16 @@ export default {
       type: Number,
       required: false,
       default: 0,
+    },
+    selected: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    closed: {
+      type: Boolean,
+      required: false,
+      default: false,
     }
   }
 }
@@ -25,5 +35,13 @@ export default {
   border: 1px solid #2c3e50;
   width: 48px;
   height: 48px;
+}
+
+.selected {
+  background: #00ff0077;
+}
+
+.closed {
+  background: #00ff0022;
 }
 </style>
